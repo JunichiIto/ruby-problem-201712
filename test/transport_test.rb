e2a3f7a6fa55a1dd7e426fe790fe55e3ef45a37e
Junichi Ito <jit@sonicgarden.jp>
2017-11-25 15:55:34 +0900

    Fix test

--- test/transport_test.rb
+++ test/transport_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require './lib/transport'
 
 class TransportTest < MiniTest::Test
-  def test_transpose
+  def test_transport
     input = "1 2 3\n4 5 6\n7 8 9"
     output = "1 4 7\n2 5 8\n3 6 9"
     assert_equal output, transport(input)
