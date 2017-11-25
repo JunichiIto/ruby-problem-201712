7b5aa225f227bd5257d4639df895686f73dedb41
Yoshiyuki Hirano <yhirano@me.com>
2017-11-26 00:09:38 +0900

    Use Array#transpose
    転置処理には、Array#transposeを使いましょう

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,7 @@
 def transport(source)
   array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
 
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
+  transported_array = array.transpose
 
   transported_array.map {|s| s.join(" ")}.join("\n")
 end
