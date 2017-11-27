d15526caaf4775d7818ecc1d0c5f519ef17ed13b
yuemori <yuemori@aiming-inc.com>
2017-11-27 10:08:32 +0900

    Add raise error on element size difference

--- lib/transport.rb
+++ lib/transport.rb
@@ -2,6 +2,8 @@ def transport(source)
   array = source.split("\n").map {|s| s.split(" ")}
   rows_count = array.first.count
 
+  raise ArgumentError, "element size differs" if array.any? { |a| a.count != rows_count }
+
   transported_array = []
   0.upto(rows_count - 1) do |i|
     transported_array << array.map {|a| a[i]}
--- test/transport_test.rb
+++ test/transport_test.rb
@@ -11,4 +11,18 @@ class TransportTest < MiniTest::Test
     output = "1 4 7 10\n2 5 8 11\n3 6 9 12"
     assert_equal output, transport(input)
   end
+
+  def test_transport_with_difference_element_size
+    input = "1 2\n4 5 6\n7 8 9"
+
+    assert_raises(ArgumentError) do
+      transport(input)
+    end
+
+    input = "1 2 3 4 5\n1 2 3 4"
+
+    assert_raises(ArgumentError) do
+      transport(input)
+    end
+  end
 end
