8b2e313bd0ffd6646c65b830844fb85fa807a782
yuemori <yuemori@aiming-inc.com>
2017-11-27 10:23:02 +0900

    Refactor use to Array#transpose

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,13 +1,7 @@
 def transport(source)
   array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
 
-  raise ArgumentError, "element size differs" if array.any? { |a| a.count != rows_count }
-
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
-
-  transported_array.map {|s| s.join(" ")}.join("\n")
+  array.transpose.map {|s| s.join(" ")}.join("\n")
+rescue IndexError => e
+  raise ArgumentError, e
 end
