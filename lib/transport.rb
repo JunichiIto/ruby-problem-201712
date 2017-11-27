e078249231d972c5e8fa8bd9b2e54d9f0a191000
Leon <leopard10000@gmail.com>
2017-11-27 22:41:39 +0900

    本体のリファクタリング
    * Array.transpose を使う
    * array という変数名は Array（クラス名）と紛らわしいので変更
    * より意味が伝わりやすいよう、split(¥n) から each_line に変更

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,7 @@
 def transport(source)
-  array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
+  org_array = source.each_line.map {|s| s.split(" ")}
 
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
+  transported_array = org_array.transpose
 
   transported_array.map {|s| s.join(" ")}.join("\n")
 end
