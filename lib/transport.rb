384c5d8aa57bb33be7e53eb2ee58e5d49103f549
Leon <leopard10000@gmail.com>
2017-12-05 17:59:59 +0900

    本体のリファクタリング
    * メソッドチェーン化

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,7 +1,8 @@
 def transport(source)
-  org_array = source.each_line.map {|s| s.split(" ")}
-
-  transported_array = org_array.transpose
-
-  transported_array.map {|s| s.join(" ")}.join("\n")
+  source
+      .each_line
+      .map {|s| s.split(" ")}
+      .transpose
+      .map {|s| s.join(" ")}
+      .join("\n")
 end
