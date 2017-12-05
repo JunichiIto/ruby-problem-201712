be8a8fa6ea69132175e64619edf97152bc805f2e
pokotyamu <pokotyamu@gmail.com>
2017-12-05 15:46:36 +0900

    change test name in more detail.

--- test/transport_test.rb
+++ test/transport_test.rb
@@ -2,11 +2,13 @@ require 'minitest/autorun'
 require './lib/transport'
 
 class TransportTest < MiniTest::Test
-  def test_transport
+  def test_transport_3x3
     input = "1 2 3\n4 5 6\n7 8 9"
     output = "1 4 7\n2 5 8\n3 6 9"
     assert_equal output, transport(input)
+  end
 
+  def test_transport_3x4
     input = "1 2 3\n4 5 6\n7 8 9\n10 11 12"
     output = "1 4 7 10\n2 5 8 11\n3 6 9 12"
     assert_equal output, transport(input)
