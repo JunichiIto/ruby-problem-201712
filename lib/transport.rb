b2c6d86cb396ad7571fa3c92b31e927fd4650b64
Leon <leopard10000@gmail.com>
2017-12-05 20:48:19 +0900

    &: を使う

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,8 +1,11 @@
+# 文字列で表現された行列を転置して、文字列形式で返す
+#   入力例: "1 2 3\n4 5 6\n7 8 9\n10 11 12"
+#   出力例: "1 4 7 10\n2 5 8 11\n3 6 9 12"
 def transport(source)
   source
       .each_line
-      .map {|s| s.split(" ")}
+      .map(&:split)
       .transpose
-      .map {|s| s.join(" ")}
+      .map {|ary| ary.join(" ")}
       .join("\n")
 end
