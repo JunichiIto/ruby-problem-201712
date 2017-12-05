98e82faee79cd7099e3ee82f706a636cee8a63a3
Leon <leopard10000@gmail.com>
2017-12-05 20:45:37 +0900

    [テストコード] ヒアドキュメントが乱用気味なのでもうちょい意図が伝わりやすいように記述する。

--- test/transport_test.rb
+++ test/transport_test.rb
@@ -8,11 +8,12 @@ class TransportTest < MiniTest::Test
       4 5 6
       7 8 9
     EOS
-    assert_equal <<~EOS.chomp, transport(input)
+    expect = <<~EOS.chomp
       1 4 7
       2 5 8
       3 6 9
     EOS
+    assert_equal expect, transport(input)
 
     input = <<~EOS.chomp
       1 2 3
@@ -20,10 +21,13 @@ class TransportTest < MiniTest::Test
       7 8 9
       10 11 12
     EOS
-    assert_equal <<~EOS.chomp, transport(input)
+    expect = <<~EOS.chomp
       1 4 7 10
       2 5 8 11
       3 6 9 12
     EOS
+    assert_equal expect, transport(input)
+
+    # assert_equal "1 4\n2 5\n3 6", transport("1 2\r\n3 4\r5 6")
   end
 end
