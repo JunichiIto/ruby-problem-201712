03dc0daa2c6c67b347e346d5a1f08d3028cef0ac
matsutani <yushiro.ma2ta2.21@gmail.com>
2017-12-06 18:28:20 +0900

    変数名修正

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,4 +1,4 @@
 def transport(source)
-  array = source.split("\n").map {|s| s.split(" ")}
-  array.transpose.map{|ary| ary.join(" ")}.join("\n")
+  matrix = source.split("\n").map {|s| s.split(" ")}
+  matrix.transpose.map{|ary| ary.join(" ")}.join("\n")
 end
