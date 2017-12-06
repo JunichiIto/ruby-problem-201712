19a999c14231c1037f540ab6a9b965e544fcd4fb
yuki3738 <y.minamiya@gmail.com>
2017-12-06 16:07:07 +0900

    Refactor transport.rb

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,15 @@
 def transport(source)
-  array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
+  two_dimensional_arrays = string_to_arrays(source)
+  transported_arrays = two_dimensional_arrays.transpose
+  arrays_to_string(transported_arrays)
+end
 
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
+private
 
-  transported_array.map {|s| s.join(" ")}.join("\n")
+def string_to_arrays(source)
+  source.split("\n").map(&:split)
+end
+
+def arrays_to_string(arrays)
+  arrays.map {|array| array.join(" ")}.join("\n")
 end
