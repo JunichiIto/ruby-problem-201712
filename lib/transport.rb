23a8b21315724d0c52d7d0020e660163608c5fad
matzryo <matzryo@example.com>
2017-12-06 20:32:25 +0900

    書き直す

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,4 @@
 def transport(source)
-  array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
-
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
-
-  transported_array.map {|s| s.join(" ")}.join("\n")
+  matrix = source.lines.map(&:split)
+  matrix.transpose.map{|row| row.join(" ")}.join("\n")
 end
