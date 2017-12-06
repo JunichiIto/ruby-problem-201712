c509d274b81e4444cf9eb826b88f0a974a903ae9
matsutani <yushiro.ma2ta2.21@gmail.com>
2017-12-06 18:21:30 +0900

    refactoring

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,4 @@
 def transport(source)
   array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
-
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
-
-  transported_array.map {|s| s.join(" ")}.join("\n")
+  array.transpose.map{|ary| ary.join(" ")}.join("\n")
 end
