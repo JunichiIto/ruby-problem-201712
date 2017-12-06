367afdaee92aed58312d74f42d05580657edfb07
ujihisa <ujihisa@gmail.com>
2017-12-06 15:14:11 +0900

    Use yet another solution
    * NOTE: this code could be even better if you do followings
        * Wrap this method into a new module, depending on the situation.
          e.g. `Xxx.transport(source)`
        * Use property-based tests such as quickcheck

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,6 @@
+require 'shellwords'
+require 'csv'
+
 def transport(source)
-  array = source.split("\n").map {|s| s.split(" ")}
-  rows_count = array.first.count
-
-  transported_array = []
-  0.upto(rows_count - 1) do |i|
-    transported_array << array.map {|a| a[i]}
-  end
-
-  transported_array.map {|s| s.join(" ")}.join("\n")
+  CSV.parse(source, col_sep: ' ').transpose.map(&:shelljoin).join(?\n)
 end
